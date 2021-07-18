00c65056cc147caa9ae08f1c8326144d36846021
wiktorpp <grazynapp65@gmail.com>
2021-07-18 22:56:35 +0200

    Added transparency to pillow converter

--- pillow.py
+++ pillow.py
@@ -4,10 +4,19 @@ im = Image.open(sys.argv[1])
 width, height = im.size
 px = im.load()
 
+trIm = Image.open(sys.argv[2])
+width, height = trIm.size
+trIm = trIm.convert("L")
+trPx = trIm.load()
+
 print("[", end="")
 for i in range(height):
     print("[", end="")
     for j in range(width):
-        print(str(px[j,i]) + ", ", end="")
+        if trPx[j, i] == 0:
+            print("0, ", end="")
+        else:
+            print(str(px[j,i]) + ", ", end="")
     print("],")
 print("]", end="")
+#import pdb; pdb.set_trace()
