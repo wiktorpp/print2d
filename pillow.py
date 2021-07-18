6d2930b31973d4117af002ecc598dc53f2bd507d
wiktorpp <grazynapp65@gmail.com>
2021-07-18 23:16:23 +0200

    Made transparency optional in pillow converter

--- pillow.py
+++ pillow.py
@@ -3,17 +3,20 @@ import sys
 im = Image.open(sys.argv[1])
 width, height = im.size
 px = im.load()
-
-trIm = Image.open(sys.argv[2])
-width, height = trIm.size
-trIm = trIm.convert("L")
-trPx = trIm.load()
+if len(sys.argv) == 3:
+    trIm = Image.open(sys.argv[2])
+    width, height = trIm.size
+    trIm = trIm.convert("L")
+    trPx = trIm.load()
+    tr = True
+else:
+    tr = False
 
 print("[", end="")
 for i in range(height):
     print("[", end="")
     for j in range(width):
-        if trPx[j, i] == 0:
+        if tr and trPx[j, i] == 0:
             print("0, ", end="")
         else:
             print(str(px[j,i]) + ", ", end="")
