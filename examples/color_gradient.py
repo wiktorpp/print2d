5fa170fe353a7b6f0ecff2719f8b53cd43b8d75c
wiktorpp <grazynapp65@gmail.com>
2021-07-21 16:30:07 +0200

    Updated color_gradient.py

--- examples/color_gradient.py
+++ examples/color_gradient.py
@@ -1,8 +1,11 @@
 from printBmp import *
 import colorsys
-
-for i in range(100): 
-    color = colorsys.hsv_to_rgb(i/100,1,1)
-    color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
-    print(pixelsToAnsi((color, color)), end="")
-print(reset)
+import os
+terminal_size = os.get_terminal_size()
+print(terminal_size.columns)
+for i in range(terminal_size.lines):
+    for i in range(os.get_terminal_size().columns): 
+        color = colorsys.hsv_to_rgb(i/100,1,1)
+        color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
+        print(pixelsToAnsi((color, color)), end="")
+    print(reset)
