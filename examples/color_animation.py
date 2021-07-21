0deb0eea107c2fac48be0d04e0f8abf5715a59d1
wiktorpp <grazynapp65@gmail.com>
2021-07-21 16:53:22 +0200

    color_animation.py exits cleanly.

--- examples/color_animation.py
+++ examples/color_animation.py
@@ -1,9 +1,17 @@
 from printBmp import *
 import colorsys
 import time
-while True:
-    for i in range(100): 
-        time.sleep(0.03)
-        color = colorsys.hsv_to_rgb(i/100,1,1)
-        color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
-        print(f"{esc}[D" + pixelsToAnsi((color, color)), end="", flush=True)
+import signal
+
+signal.signal(signal.SIGINT, lambda sig, frame: [print(reset), exit()])
+print("Press Ctrl-C to exit.")
+
+try:
+    while True:
+        for i in range(100): 
+            time.sleep(0.03)
+            color = colorsys.hsv_to_rgb(i/100,1,1)
+            color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
+            print(f"{esc}[D" + pixelsToAnsi((color, color)), end="", flush=True)
+except:
+    pass
