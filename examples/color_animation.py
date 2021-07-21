af4b157de890ecbad587f0815a8d7a656b50f83c
wiktorpp <grazynapp65@gmail.com>
2021-07-21 16:44:10 +0200

    Added color_animation.py

---
+++ examples/color_animation.py
@@ -0,0 +1,9 @@
+from printBmp import *
+import colorsys
+import time
+while True:
+    for i in range(100): 
+        time.sleep(0.03)
+        color = colorsys.hsv_to_rgb(i/100,1,1)
+        color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
+        print(f"{esc}[D" + pixelsToAnsi((color, color)), end="", flush=True)
