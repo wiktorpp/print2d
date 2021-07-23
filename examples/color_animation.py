36d75a2bd8ff7dbc108e63f61c615f68d298ba23
wiktorpp <grazynapp65@gmail.com>
2021-07-23 17:04:23 +0200

    Updated examples

--- examples/color_animation.py
+++ examples/color_animation.py
@@ -8,6 +8,6 @@ try:
             time.sleep(0.03)
             color = colorsys.hsv_to_rgb(i/100,1,1)
             color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
-            print(f"{esc}[D" + pixelsToAnsi((color, color)), end="", flush=True)
+            print(f"{ansi.esc}[D" + pixelsToAnsi((color, color)), end="", flush=True)
 except:
-    print(reset)
+    print(ansi.reset)
--- examples/color_gradient.py
+++ examples/color_gradient.py
@@ -9,4 +9,4 @@ for i in range(terminal_size.lines):
         color = colorsys.hsv_to_rgb(i/100,1,1)
         color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
         print(pixelsToAnsi((color, color)), end="")
-    print(reset)
+    print(ansi.reset)
