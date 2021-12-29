085db76054a091f17d58f51c7460bffe3ebf8afa
wiktorpp <grazynapp65@gmail.com>
2021-12-29 16:55:14 +0100

    Refactored variable names

--- convert.py
+++ convert.py
@@ -4,38 +4,38 @@ import sys
 monochrome = False
 
 try: 
-    imFilename = sys.argv[1]
+    imageFilename = sys.argv[1]
 except:
     print("usage: ./convert.py <image> [transparency] > <output>")
     exit()
 
-im = Image.open(imFilename)
-width, height = im.size
+image = Image.open(imageFilename)
+width, height = image.size
 if monochrome:
-    im = im.convert("L")
-px = im.load()
+    image = image.convert("L")
+pixelmap = image.load()
 try:
-    trIm = Image.open(sys.argv[2])
+    transparencyImage = Image.open(sys.argv[2])
 except:
-    tr = False
+    transparency = False
 else:
-    trIm = trIm.convert("L")
-    trPx = trIm.load()
-    tr = True
+    transparencyImage = transparencyImage.convert("L")
+    transparencyPixelmap = transparencyImage.load()
+    transparency = True
 
 bmp = []
 for i in range(height):
     row = []
     for j in range(width):
         if monochrome:
-            if px[j,i] == 255:
+            if pixelmap[j,i] == 255:
                 row.append(1)
             else:
                 row.append(0)
-        elif tr and trPx[j, i] == 0:
+        elif transparency and transparencyPixelmap[j, i] == 0:
             row.append(0)
         else:
-            row.append(px[j,i])
+            row.append(pixelmap[j,i])
     bmp.append(row)
 
 from printBmp import bmpToAnsi
